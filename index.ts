0e6a90fa9b3619512cf622aa7b6f10f458188dbe
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-25 21:54:41 +0300

    filter

--- index.ts
+++ index.ts
@@ -1,5 +1,24 @@
 import { JumpFm } from 'jumpfm-api'
 
 export const load = (jumpFm: JumpFm) => {
+    jumpFm.panels.forEach(panel => {
+        panel.bind('showFilter', ['f'], () =>
+            panel.filterBox.focus()
+        )
 
+        panel.bind('clearFilter', ['esc'], () => {
+            panel.filterBox.clear()
+            panel.filterRemove('filter')
+        })
+
+        panel.filterBox.bind('hideFilter', ['esc'], panel.filterBox.hide)
+
+        panel.filterBox.onChange(pattern => {
+            if (!pattern) {
+                panel.filterRemove('filter')
+                return
+            }
+            panel.filterSet('filter', item => item.name.indexOf(pattern) >= 0)
+        })
+    })
 }
